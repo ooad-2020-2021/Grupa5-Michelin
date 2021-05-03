237a576e2268532d624ab327d157794bd159be20
ahadzibajr1 <73421547+ahadzibajr1@users.noreply.github.com>
2021-05-03 09:36:05 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Michelin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
